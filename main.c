23822bd7aa79e483be36983d9d14684b77435a01
marc.lallias <marc.lallias@epitech.eu>
2017-01-24 14:22:06 +0100

    lol

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for  in /home/darkmark/rendu/PSU_2016_malloc
+** 
+** Made by DarKmarK
+** Login   <marc.lallias@epitech.eu>
+** 
+** Started on  Tue Jan 24 12:06:12 2017 DarKmarK
+** Last update Tue Jan 24 12:23:43 2017 DarKmarK
+*/
+
+#include "my_alloc.h"
+
+int	main()
+{
+  return (0);
+}
---
+++ my_malloc.c
@@ -0,0 +1,16 @@
+/*
+** my_malloc.c for  in /home/darkmark/rendu/PSU_2016_malloc
+** 
+** Made by DarKmarK
+** Login   <marc.lallias@epitech.eu>
+** 
+** Started on  Tue Jan 24 12:08:28 2017 DarKmarK
+** Last update Tue Jan 24 12:28:01 2017 DarKmarK
+*/
+
+#include "my_alloc.h"
+
+void	*malloc(size_t size)
+{ 
+  return (NULL);
+}
