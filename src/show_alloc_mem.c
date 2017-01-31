57a5975b087805b91d95eb4bbff93c563e62ec20
Peixoto Pierre <pierre.peixoto@epitech.eu>
2017-01-31 13:48:58 +0100

    pushing test of show elements allocated

--- src/show_alloc_mem.c
+++ src/show_alloc_mem.c
@@ -5,32 +5,43 @@
 ** Login   <marc.lallias@epitech.eu>
 ** 
 ** Started on  Tue Jan 31 10:21:25 2017 DarKmarK
-** Last update Tue Jan 31 10:22:02 2017 DarKmarK
+** Last update Tue Jan 31 11:08:58 2017 pierre.peixoto
 */
 
 #include "../header/malloc.h"
 
+static int	my_putchar(const char c)
+{
+  return ((write(1, &c, 1) == 0) ? 0 : -1);
+}
+
+static int	my_put_nbr(const int nb)
+{
+  if (nb < 0)
+    {
+      if (write(1, "-", 1) == -1)
+	return (-1);
+      nb = nb * -1;
+    }
+  if (nb / 10)
+    my_put_nbr(nb / 10);
+  my_putchar((nb % 10) + 48);
+  return (0);
+}
+
 void		show_alloc_mem(void)
 {
-  t_meta_data	*offset;
-  void		*cast_add;
+  t_meta_data	*begin;
 
-  
-  //printf("sizeof(t_meta_data) %d break %d page size %d\n", (int)SIZE_META_DATA, (int)sbrk(0), (int)PAGE_SIZE);
+  begin = start;
   if (start == NULL)
     return;
-  offset = start; //Pour le premier
-  while ((offset != NULL) && (offset != end))
+  printf("break: %p\n", end);
+  while (start != NULL)
     {
-      cast_add = offset;
-      /*
-      if (offset->next == NULL)
-	printf("%d - %d : %d bytes   diff %d\n", offset, offset + offset->size + SIZE_META_DATA, 666);
-	else
-	  printf("%d - %d : %d bytes   free %d\n", offset, offset->next, offset->size, offset->is_free);
-      */
-      offset = offset->next;
+      printf("%p - %p : %d bytes\n", start + SIZE_META_DATA, start + SIZE_META_DATA + start->size,
+	     (int)start->size);
+      start = start->next;
     }
-
-  return ;
+  start = begin;
 }
