d234b99b176b3ebea9a37200aad0142bcf589838
Peixoto Pierre <pierre.peixoto@epitech.eu>
2017-01-31 14:11:32 +0100

    adding show elem corrected, but ajustments already needed

--- src/show.c
+++ src/show.c
@@ -5,5 +5,21 @@
 ** Login   <marc.lallias@epitech.eu>
 ** 
 ** Started on  Sat Jan 28 21:22:06 2017 DarKmarK
-** Last update Sat Jan 28 21:22:07 2017 DarKmarK
+** Last update Tue Jan 31 14:10:39 2017 pierre.peixoto
 */
+
+void		show_alloc_mem(void)
+{
+  t_meta_data	*begin;
+
+  if (start == NULL)
+    return ;
+  printf("break: %p\n", end);
+  begin = start;
+  while (start != NULL)
+    {
+      printf("%p - %p : %d bytes\n", start + SIZE_META_DATA, start + SIZE_META_DATA + start->size);
+      start = start->next;
+    }
+  start = begin;
+}
