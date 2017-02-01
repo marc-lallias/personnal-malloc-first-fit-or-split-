3a25e35f7d3d1e3488458dccec2f079a93dff39f
Peixoto Pierre <pierre.peixoto@epitech.eu>
2017-02-01 12:26:00 +0100

    hotfix on show

--- src/show.c
+++ src/show.c
@@ -5,7 +5,7 @@
 ** Login   <marc.lallias@epitech.eu>
 ** 
 ** Started on  Sat Jan 28 21:22:06 2017 DarKmarK
-** Last update Tue Jan 31 16:43:13 2017 pierre.peixoto
+** Last update Wed Feb  1 12:15:47 2017 pierre.peixoto
 */
 
 #include "../header/malloc.h"
@@ -15,7 +15,7 @@ static int	my_putchar(const char c)
   return ((write(1, &c, 1) == 0) ? 0 : -1);
 }
 
-void			my_put_nbr_hexa(const unsigned int nb)
+void		my_put_nbr_hexa(const size_t nb)
 {
   char			hexa[16] = { '0', '1', '2', '3', '4', '5', '6',
 				     '7', '8', '9', 'A', 'B', 'C', 'D',
@@ -38,18 +38,27 @@ void		my_put_nbr(const unsigned int nb)
 void		show_alloc_mem(void)
 {
   t_meta_data	*begin;
+  size_t	addr;
 
   if (start == NULL)
     return ;
+  addr = (size_t)sbrk(0);
+  my_put_nbr(addr);
+  write(1, "\n", 1);
   write(1, "break: ", 7);
-  my_put_nbr((unsigned int)end);
+  addr = (size_t)(end);
+  my_put_nbr(addr);
   write(1, "\n", 1);
   begin = start;
   while (start != end)
     {
-      my_put_nbr((unsigned int)(start + SIZE_META_DATA));
+      my_put_nbr((unsigned int)(SIZE_META_DATA));
+      write(1, "\n", 1);
+      addr = (size_t)start + SIZE_META_DATA;
+      my_put_nbr(addr);
       write(1, " - ", 3);
-      my_put_nbr((unsigned int)(start->next));
+      addr = (size_t)start->next;
+      my_put_nbr(addr);
       write(1, " : ", 3);
       my_put_nbr(start->size);
       write(1, " bytes\n", 7);
